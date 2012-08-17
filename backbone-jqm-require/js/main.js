fc8408fdc2406adc62cac2683e17875b6e050ea1
Andrew Henderson <andrew.henderson@chromeriver.com>
2012-08-17 16:06:19 -0700

    Configured jQM

--- backbone-jqm-require/js/main.js
+++ backbone-jqm-require/js/main.js
@@ -1,6 +1,20 @@
 require.config({
 	paths: {
-		jquery: 'vendor/jqm/jquery_1.8_min',
-		jqm: 'vendor/jqm/jquery'
+		jquery: 	'vendor/jqm/jquery_1.8_min',
+		jqm: 		'vendor/jqm/jquery.mobile-1.1.1',
+		underscore: 'vendor/underscore/underscore_amd',
+		backbone: 	'vendor/backbone/backbone_amd',
+		text: 		'vendor/require/text',
+		plugin: 	'plugin',
+		templates: 	'../templates',
+		modules: 	'../modules',
+		model: 		'../model'
 	}
-})
+});
+
+define(['app', 'jqm-config'], function(app){
+	$(function(){
+		console.log("DOM IS READY");
+	});
+	app.initialize();
+});
