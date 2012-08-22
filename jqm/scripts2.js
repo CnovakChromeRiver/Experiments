575fd75704cff0499758811d660d84d258dd865d
Andrew Henderson <andrew.henderson@chromeriver.com>
2012-08-22 15:00:30 -0700

    Archiving script2.js

--- jqm/scripts2.js
+++ jqm/scripts2.js
@@ -1,47 +1,90 @@
 $(function () {
+	
+	var timeStamp = new Date().getTime();
+	
 	$(document).on("mousewheel", "a", function (event, delta) {
-		if (delta > 0) {
-			$.mobile.changePage($(this).attr("href"), { transition: "pop" });
+
+		event.preventDefault();
+		
+		var timeNow = new Date().getTime();
+
+		if(timeNow - timeStamp < 100) {
+			timeStamp = timeNow;
+			return;
+		} else {
+			timeStamp = timeNow;
+			if (delta > 0) {
+				$.mobile.changePage($(this).attr("href"), { transition: "pop" });
+			}
 		}
 	});
-	$(document).on("mousewheel", document, function (event, delta) {
-		if (delta < 0) {
+	
+	$(document).on("mousewheel", function (event, delta) {
 
-			var parent = $(".page").attr("data-parent");
-			if (parent !== undefined) {
-				$.mobile.changePage(parent, { transition: "pop" });
+		event.preventDefault();
+
+		var timeNow = new Date().getTime();
+		
+		if(timeNow - timeStamp < 100) {
+			timeStamp = timeNow;
+			return;
+		} else {
+			timeStamp = timeNow;
+			if (delta < 0) {
+				var parent = $(".page").attr("data-parent");
+				if (parent !== undefined) {
+					$.mobile.changePage(parent, { transition: "pop" });
+				}
 			}
 		}
 	});
 
-	document.addEventListener('gesturechange', function(event) {
+	document.addEventListener('gesturechange', function (event) {
+
 	    event.preventDefault();
-	    //$(".target").text(event.target);
-	    $(".measure").text("Scale: " + event.scale + ", Rotation: " + event.rotation);
-	    var href = $(this).attr("href");
-	    if(event.scale > 1) {
+
+	    var scale = event.scale;
+
+	    //$(".measure").text(event.scale);
+
+	    if(scale > 1) {
 	    	$(".behavior").text("Zooming In");
-	    	$(".target").text(href);
-	    	if($(this).attr("href") !== undefined) {
-	    		$.mobile.changePage($(this).attr("href"), { transition: "pop" });
-	    	}
-	    }
-	    else {
+	    } else {
 	    	$(".behavior").text("Zooming Out");
-	    	$(".target").text(href);
-	    	if($(".page").attr("href") !== undefined) {
-	    		$.mobile.changePage($(".page").attr("href"), { transition: "pop" });
-	    	}
 	    }
+
+	    gestureEnd(scale);
+
 	}, false);
 
-	//$("a").on('gesturechange', function(event) {
-		//$(".jquery-reading").text("Positive");
-		//$.mobile.changePage($(this).attr("href"), { transition: "pop" });
-		//event.preventDefault();
-		//$(this).css("background", "red");
-		//if(event.scale > 1) {
-	  //  	$(".jquery-reading").text("Navigating");
-	  //  }
-	//});
+	function gestureEnd(s) {
+
+		var scale = s;
+		
+		$("body").on('gestureend', "a", function (event, scale) {
+
+			$(".measure").text(scale);
+
+			//$(".jquery-reading").text("Positive");
+
+
+			var parent = $(".page").attr("data-parent");
+			
+
+			if(scale > 1 && event.target !== undefined) {
+		    	$(".behavior").text("Drilling Down...");
+		    } else if (scale < 1 && parent !== undefined) {
+		    	$(".behavior").text("Navigating to the parent...");
+		    } else {
+		    	// do nothing
+		    }
+			//$.mobile.changePage($(this).attr("href"), { transition: "pop" });
+			/* event.preventDefault();
+			$(this).css("background", "red");
+			if(event.scale > 1) {
+		    	$(".jquery-reading").text("Navigating");
+		    }*/
+		});
+	}
+
 });
