a88d41e6084b5de445d2b0977913a23c2cf84a4e
Andrew Henderson <andrew.henderson@chromeriver.com>
2012-08-21 23:05:28 -0700

    More Gesture Change Tests

--- jqm/scripts2.js
+++ jqm/scripts2.js
@@ -16,20 +16,32 @@ $(function () {
 
 	document.addEventListener('gesturechange', function(event) {
 	    event.preventDefault();
+	    //$(".target").text(event.target);
 	    $(".measure").text("Scale: " + event.scale + ", Rotation: " + event.rotation);
+	    var href = $(this).attr("href");
 	    if(event.scale > 1) {
 	    	$(".behavior").text("Zooming In");
+	    	$(".target").text(href);
+	    	if($(this).attr("href") !== undefined) {
+	    		$.mobile.changePage($(this).attr("href"), { transition: "pop" });
+	    	}
 	    }
-	    else { $(".behavior").text("Zooming Out"); }
+	    else {
+	    	$(".behavior").text("Zooming Out");
+	    	$(".target").text(href);
+	    	if($(".page").attr("href") !== undefined) {
+	    		$.mobile.changePage($(".page").attr("href"), { transition: "pop" });
+	    	}
+	    }
 	}, false);
 
-	$("a").on('gesturechange', function(event) {
-		$(".jquery-reading").text("Positive");
-		$.mobile.changePage($(this).attr("href"), { transition: "pop" });
-		/* event.preventDefault();
-		$(this).css("background", "red");
-		if(event.scale > 1) {
-	    	$(".jquery-reading").text("Navigating");
-	    }*/
-	});
+	//$("a").on('gesturechange', function(event) {
+		//$(".jquery-reading").text("Positive");
+		//$.mobile.changePage($(this).attr("href"), { transition: "pop" });
+		//event.preventDefault();
+		//$(this).css("background", "red");
+		//if(event.scale > 1) {
+	  //  	$(".jquery-reading").text("Navigating");
+	  //  }
+	//});
 });
